e09369e139811cb65d1d94dc505a202ddeebaf45
beanstown106 <nbenis106@gmail.com>
2017-08-29 12:14:39 +0200

    [2/2] frameworks_base: Lockscreen keypad ripple
    Add ability to disable ripple when entering lockscreen pin
    
    Change-Id: I8f1a8a319473be99bf3d5b48ab9cd11f8ee5e77e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4171,6 +4171,12 @@ public final class Settings {
         public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
 
         /**
+         * Whether to disable ripple on lockscreen pin entry
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_RIPPLE = "lockscreen_pin_ripple";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
